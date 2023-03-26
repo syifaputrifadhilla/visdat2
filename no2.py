004b2e540198a6f59254ccbd43f9ad75a54976e1
syifaputrifadhilla <128953636+syifaputrifadhilla@users.noreply.github.com>
2023-03-26 12:40:07 +0700

    Update no2.py

--- no2.py
+++ no2.py
@@ -1,30 +1,43 @@
-import pandas as pd
-import plotly.express as px
+mport plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
 
-with st.sidebar:
-      X = st.selectbox(
-      'X',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
-      Y = st.selectbox(
-      'Y',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
-      Size = st.selectbox(
-      'Size',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
-      Color = st.selectbox(
-      'Color',
-      ('  GDP per capita ', ' Life expectancy at birth', '  Population ',' Birth rate'))
-      Circle_area = st.slider('Circle Area', 0, 100, 60)
 
-      st.write('X:', X)
-      st.write('Y:', Y)
-      st.write('Size:', Size)
-      st.write('Color:', Color)
-      st.write("Circle Area:", Circle_area)
+with st.expander("Eksplorasi Data"):
+    st.snow()
+    st.title('Configure Plot')
+    st.header('Silahkan Eksplorasi')
+    option_x = st.selectbox(
+        '',
+        (' gdppercap ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' Country ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('X :', option_x)
 
-Data = pd.read_csv("factbook.csv")
+    option_y = st.selectbox(
+        '',
+        (' lifeexpectatbirth ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' Country ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Y :', option_y)
 
-fig = px.scatter(data_frame=Data,x=X, y=Y,size =Size,color=Color,log_x=True,log_y=True,size_max=Circle_area)
+    option_size = st.selectbox(
+        '',
+        (' population ', '  Area ', ' birthrate ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' Country ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Size :', option_size)
 
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+    option_color = st.selectbox(
+        '',
+        (' birthrate ', '  Area ', ' Country ', '   Current account balance  ', '  Death rate ', '  Electricity consumption ', '   Electricity production  ', '   Exports  ', '   GDP  ', ' gdppercap ', '  GDP real growth rate ', '   Highways  ', '   Imports  ', '  Industrial production growth rate ', '  Infant mortality rate ', '  Inflation rate  ', '   Internet users  ', '  Investment ', '   Labor force  ', ' lifeexpectatbirth ', '  Military expenditures ', '   Natural gas consumption  ', '   Oil consumption  ', ' population ', '  Public debt ', '  Railways ', '   Reserves of foreign exchange & gold  ', '  Total fertility rate ', '  Unemployment rate '))
+    st.write('Color :', option_color)
+
+bub = st.slider('Ukuran Bubble', 1, 100, 60)
+df = pd.read_csv('factbook.csv')
+fig1 = px.scatter(df,
+    x=option_x,
+    y=option_y,
+    size=option_size,
+    color=option_color,
+    size_max=bub,
+    hover_data=[option_size],
+   color_continuous_scale=px.colors.sequential.Viridis,
+)
+st.plotly_chart(fig1)
